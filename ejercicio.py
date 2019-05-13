28199ddff109f8e8c96f139b23148c1110024968
laura <laugo27032002@gmail.com>
2019-05-13 17:02:17 -0400

    soy feliz

---
+++ ejercicio.py
@@ -0,0 +1,13 @@
+######################
+#comandos de GIT
+######################
+#git add.
+#git commit -m "el mensaje"
+#git push
+
+#comandos para navegar en la terminal
+####################
+
+#ls o dir   (para ver las carpetas/archivos existentes)
+#cd nombre_de_archivos  (Para entrar en la carpeta/archivo)
+#cd ..   (para retroceder de/la carpeta/archivo)
